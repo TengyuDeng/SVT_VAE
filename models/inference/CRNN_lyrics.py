8b5c56ea955a1db9b27a1a4d3d48cee2db552ae3
Deng Tengyu <deng@sap.ist.i.kyoto-u.ac.jp>
2022-07-21 14:27:51 +0900

    add lyrics part

---
+++ models/inference/CRNN_lyrics.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+from ..basic.CNN import ConvNN
+from ..basic.RNN import RNNLayer
+
+def downsample_length(orig_length, down_sample):
+    if isinstance(orig_length, torch.Tensor):
+        return torch.div(orig_length - 1, down_sample, rounding_mode='floor') + 1
+    else:
+        return (orig_length - 1) // down_sample + 1
+def get_padding(kernel_size):
+    if isinstance(kernel_size, int):
+        padding = kernel_size // 2
+    else:
+        padding = tuple(x // 2 for x in kernel_size)
+    return padding
+
+class CRNN_lyrics(nn.Module):
+
+    def __init__(
+        self,
+        num_classes=72,
+        input_features=128,
+        input_channels=1,
+        num_convs=5,
+        conv_channels=[64, 32, 32, 32, 32,],
+        kernel_sizes=[5, 5, 3, 3, 3,],
+        dropout=0.,
+        num_lstms=2,
+        lstm_channels=512,
+        down_sample=2,
+        **args
+        ):
+
+        super().__init__()
+
+        self.num_classes = num_classes
+        self.input_features = input_features
+        
+        self.input_channels = input_channels
+        self.conv_channels = conv_channels
+        self.kernel_sizes = kernel_sizes
+        self.down_sample = down_sample
+        
+        self.cnn = ConvNN(
+            input_channels=input_channels,
+            num_convs=num_convs,
+            conv_channels=conv_channels,
+            kernel_sizes=kernel_sizes,
+            dropout=dropout,
+        )
+        self.pooling = nn.MaxPool2d(
+            kernel_size=3, 
+            stride=self.down_sample, 
+            padding=get_padding(3),
+        )
+
+        self.rnn = nn.Sequential(
+            *[
+            RNNLayer(
+            input_size=lstm_channels * 2 if i > 0 else conv_channels[-1] * downsample_length(input_features, self.down_sample), 
+            hidden_size=lstm_channels,
+            dropout=dropout,
+            )
+            for i in range(num_lstms)
+            ]
+            )
+        self.dense = nn.Sequential(
+            nn.Linear(in_features=lstm_channels * 2, out_features=lstm_channels),
+            nn.ReLU(),
+            nn.Linear(in_features=lstm_channels, out_features=num_classes),
+            )
+
+    def forward(self, x):
+        
+        if x.ndim == 3:
+            x = x.squeeze(1)
+        if x.shape[-2] != self.input_features:
+            raise ValueError(f"Number of input features not match! expected{self.input_features} but got {x.shape[-2]}")
+
+        x = self.cnn(x)
+        # x: (batch_size, num_channels=conv_channels, num_features, length)
+        x = self.pooling(x)
+        old_shape = x.shape
+        x = x.reshape(old_shape[0], old_shape[1] * old_shape[2], old_shape[3])
+        # x: (batch_size, channel * num_features, num_frames)
+
+        x = x.permute(2, 0, 1)
+        # x: (num_frames, batch_size, num_channels)
+
+        x = self.rnn(x)
+        x = self.dense(x)
+        # x: (num_frames, batch_size, num_classes)
+      
+        return x
---
+++ models/reconst/ReconstRNN.py
@@ -0,0 +1,158 @@
+import torch
+from torch import nn
+from torch_scatter import segment_csr
+from ..basic.CNN import ConvNN
+from ..basic.RNN import RNNLayer
+
+ALPHA = 1e-4
+
+def repeat_with_tatums(features, tatum_frames):
+    """
+    Input:
+      features: (batch_size, num_tatums, num_features)
+      tatum_frames: (batch_size, num_tatums + 1)
+    Return:
+      features_repeat: (batch_size, max(tatum_frames[:, -1]), num_features)
+    """
+    batch_size = features.shape[0]
+    new_features = [features[i, ...].repeat_interleave(torch.diff(tatum_frames[i]), dim=0) for i in range(batch_size)]
+    return torch.nn.utils.rnn.pad_sequence(new_features, batch_first=True)
+
+def adjust_shape(x, ref_shape):
+    """
+    Inputs: 
+      x: (batch_size, channel_x, H_x, W_x)
+      ref: (batch_size, channel_ref, H_ref, W_ref)
+    Returns:
+      x_adjusted: (batch_size, channel_ref, H_ref, W_ref)
+    Only appliable when 
+    (H_x - H_ref) * (W_x - W_ref) >= 0
+    """
+    H_x, W_x = x.shape[-2:]
+    H_ref, W_ref = ref_shape
+    if (H_x - H_ref) * (W_x - W_ref) < 0:
+        raise ValueError(f"Wrong shapes: {x.shape}, {ref_shape}")
+    
+    if H_x >= H_ref:
+        return x[..., :H_ref, :W_ref]
+    else:
+        padding = (0, W_ref - W_x, 0, H_ref - H_x)
+        return nn.ZeroPad2d(padding)(x)
+
+class RNN(nn.Module):
+
+    def __init__(
+        self,
+        num_features,
+        num_lstms=3,
+        lstm_channels=512,
+        dropout=0.,
+        **args
+        ):
+        super().__init__()
+
+        self.num_features = num_features
+        self.lstm_channels = lstm_channels
+
+        self.rnn = nn.Sequential(
+            *[
+            RNNLayer(
+            input_size=lstm_channels * 2 if i > 0 else num_features + 1, 
+            hidden_size=lstm_channels,
+            dropout=dropout,
+            )
+            for i in range(num_lstms)
+            ]
+            )
+        self.relu = torch.nn.ReLU()
+        self.dense = nn.Linear(in_features=lstm_channels, out_features=output_features)
+
+    def forward(self, pitch_rendered, onset, tatum_frames):
+        
+        # Input:
+        #   pitch: (batch_size, num_tatums, num_features)
+        #   onset: (batch_size, num_tatums)
+        # Return:
+        #   reconst_output: (batch_size, num_frames, num_features)
+        pitch_rendered = repeat_with_tatums(pitch_rendered, tatum_frames)
+        onset = repeat_with_tatums(onset[:,:,None], tatum_frames)
+        reconst_input = torch.cat([pitch_rendered, onset], dim=-1)
+        # reconst_input: (batch_size, num_frames, num_features + 1)
+        reconst_input = self.rnn(reconst_input.transpose(0, 1)).transpose(0, 1)
+        reconst_input = self.relu(reconst_input[..., :self.lstm_channels] + reconst_input[..., self.lstm_channels:])
+        reconst_output = self.dense(reconst_input)
+        
+        return reconst_output
+
+class RNNVariational(nn.Module):
+
+    def __init__(
+        self,
+        output_features,
+        z_features,
+        phoneme_features=72,
+        num_lstms=3,
+        lstm_channels=512,
+        dropout=0.,
+        features=["pitch", "onset"],
+        **args
+        ):
+
+        super().__init__()
+
+        self.output_features = output_features
+        self.features = features
+        self.lstm_channels = lstm_channels
+        num_features = {
+        "pitch": output_features,
+        "onset": 1,
+        "lyrics": phoneme_features,
+        }
+        input_features = 0
+        for feature_name in features:
+            try:
+                input_features += num_features[feature_name]
+            except KeyError:
+                raise RuntimeError(f"In constructing reconst model. No such feature {feature_name}.")
+
+        self.rnn = nn.Sequential(
+            *[
+            RNNLayer(
+            input_size=lstm_channels * 2 if i > 0 else input_features + z_features, 
+            hidden_size=lstm_channels,
+            dropout=dropout,
+            )
+            for i in range(num_lstms)
+            ]
+            )
+        self.relu = torch.nn.ReLU()
+        self.dense = nn.Linear(in_features=lstm_channels, out_features=output_features)
+        
+    def forward(self, z, pitch_rendered, onset, tatum_frames, lyrics=None, lyrics_downsample=2):
+        
+        # Input:
+        #   z: (batch_size, num_frames, z_features)
+        #   pitch: (batch_size, num_tatums, num_features)
+        #   onset: (batch_size, num_tatums)
+        #   lyrics: (batch_size, num_frames, phoneme_features)
+        # Return:
+        #   reconst_output: (batch_size, num_frames, num_features)
+        inputs = []
+        if "pitch" in self.features:
+            pitch_rendered = repeat_with_tatums(pitch_rendered, tatum_frames)
+            inputs.append(pitch_rendered)
+        if "onset" in self.features:
+            onset = repeat_with_tatums(onset[:,:,None], tatum_frames)
+            inputs.append(onset)
+        if "lyrics" in self.features:
+            lyrics = lyrics.repeat_interleave(lyrics_downsample, dim=1)
+            lyrics = adjust_shape(lyrics, ref_shape=(z.shape[-2], lyrics.shape[-1]))
+            inputs.append(lyrics)
+        inputs.append(z)
+        reconst_input = torch.cat(inputs, dim=-1)
+        # reconst_input: (batch_size, num_frames, input_features + z_features)
+        reconst_input = self.rnn(reconst_input.transpose(0, 1)).transpose(0, 1)
+        reconst_input = self.relu(reconst_input[..., :self.lstm_channels] + reconst_input[..., self.lstm_channels:])
+        reconst_output = self.dense(reconst_input)
+
+        return reconst_output
